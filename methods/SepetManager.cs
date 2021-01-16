f208448b24b121c7bdce7b0c8101702ef0eccd7a
Rahime Demir <rahimedemir2000@gmail.com>
2021-01-17 00:10:53 +0300

    16 Ocak ders sonu

---
+++ methods/SepetManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace methods
+{
+    class SepetManager
+    {
+        //naming convention --- yazım kuralı (ilk harf büyük)
+        //syntax
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi: "+ urun.Adi);
+        }
+
+        public void Ekle2(string urunAdi, string aciklama, double fiyat, int stokAdedi)
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi: " + urunAdi);
+        }
+    }
+
+}
